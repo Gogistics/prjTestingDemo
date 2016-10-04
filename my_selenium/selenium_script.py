fc179b75a7b406f620c8a6c81bb14859e25f6599
AlanTai <rainman.tai@gmail.com>
2016-10-04 13:38:09 -0700

    add mechanism of generating testing reports

--- my_selenium/selenium_script.py
+++ my_selenium/selenium_script.py
@@ -1,4 +1,5 @@
-import time
+# -*- coding: utf-8 -*-
+import time, datetime
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -10,16 +11,40 @@ def init_driver():
   driver = webdriver.Chrome('/Users/alantai/Desktop/my_selenium_fabric/chromedriver')
   driver.wait = WebDriverWait(driver, 5)
   return driver
- 
- 
-def lookup(driver, query):
+
+def test(driver, query):
   driver.get('http://www.riverbed.com/products/steelhead/index.html')
+  timestamp = str(time.time()).split('.')[0]
+  test_file = open('./reports/test-report-{timestamp}.txt'.format(timestamp=timestamp), 'a')
+
+  print '\n\n\n'
+  print '<=== Start Test Automation ===>'
+  print '\n'
+
+  # write to report
+  test_file.write('Test Report')
+  test_file.write('\n')
+  test_file.write('===========')
+  test_file.write('\n\n\n')
+
   try:
     # check title
+    print '<--- Check Title --->'
     print driver.title
+    print '\n'
+    test_file.write('<--- Check Title --->')
+    test_file.write('\n')
+    test_file.write(u''.join(driver.title).encode('utf-8'))
+    test_file.write('\n\n')
 
     # check cookies
+    print '<--- Check Cookies --->'
     print driver.get_cookies()
+    print '\n'
+    test_file.write('<--- Check Cookies --->')
+    test_file.write('\n')
+    test_file.write(driver.get_cookies().__str__())
+    test_file.write('\n\n')
 
     # click event
     search_btn = driver.find_element_by_xpath('//*[@id="tools"]/ul/li[3]/a')
@@ -42,13 +67,59 @@ def lookup(driver, query):
     time.sleep(3)
 
     search_result = driver.find_element_by_xpath('//*[@id="totalresults"]')
+    # search_result.value_of_css_property()
+    print '<--- Check Style --->'
+    print search_result.get_attribute('style')
+    print '\n'
+    test_file.write('<--- Check Style --->')
+    test_file.write('\n')
+    test_file.write(search_result.get_attribute('style').__str__())
+    test_file.write('\n\n')
+
     # check detail
+    print '<--- Check innerHTML --->'
     print search_result.get_attribute('innerHTML')
+    print '\n'
+    test_file.write('<--- Check innerHTML --->')
+    test_file.write('\n')
+    test_file.write(search_result.get_attribute('innerHTML'))
+    test_file.write('\n\n')
+
+    print '<--- Check Text of Element --->'
     print search_result.text
+    print '\n'
+    test_file.write('<--- Check Text of Element --->')
+    test_file.write('\n')
+    test_file.write(search_result.text)
+    test_file.write('\n\n')
+
+    # scroll page to bottom
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+    print '<--- Check Tag Name of Element --->'
     print search_result.tag_name
-    print search_result.parent
+    print '\n'
+    test_file.write('<--- Check Tag Name of Element --->')
+    test_file.write('\n')
+    test_file.write(search_result.tag_name)
+    test_file.write('\n\n')
+
+    print '<--- Check Location of Element --->'
     print search_result.location
+    print '\n'
+    test_file.write('<--- Check Parent of Element --->')
+    test_file.write('\n')
+    test_file.write(str(search_result.location))
+    test_file.write('\n\n')
+
+    print '<--- Check Size of Element --->'
     print search_result.size
+    print '\n'
+    test_file.write('<--- Check Size of Element --->')
+    test_file.write('\n')
+    test_file.write(str(search_result.size))
+    test_file.write('\n\n')
+
     time.sleep(3)
 
     search_btn.click()
@@ -58,11 +129,13 @@ def lookup(driver, query):
     time.sleep(3)
 
   except TimeoutException:
-    print("Box or Button not found in google.com")
+    print("Box or Button not found in riverbed.com")
+  finally:
+    test_file.close()
  
  
 if __name__ == "__main__":
   driver = init_driver()
-  lookup(driver, "stealhead")
+  test(driver, "stealhead")
   time.sleep(5)
   driver.quit()
