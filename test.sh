a63790e3848272f8abb201557d7dc2f573816d7a
Alan Tai <gogistics@gogistics-tw.com>
2016-10-25 14:33:19 -0700

    add command for executing selenium script

--- test.sh
+++ test.sh
@@ -3,4 +3,9 @@
 # renew docker contaner
 # History:
 # 09/21/2016
+
+# testing for Node.js App
 node_modules/mocha/bin/mocha test.js
+
+# testing with Selenium
+python ./my_selenium/selenium_script.py
