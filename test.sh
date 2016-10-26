45b18fa2f24cb27946563c7a6716ef090348f526
Alan Tai <gogistics@gogistics-tw.com>
2016-10-26 14:25:26 -0700

    update shell script

--- test.sh
+++ test.sh
@@ -3,9 +3,20 @@
 # renew docker contaner
 # History:
 # 09/21/2016
+export PATH=$PATH:/Users/alantai/Desktop/my_selenium_fabric/
 
-# testing for Node.js App
-node_modules/mocha/bin/mocha test.js
+option="${1}"
 
-# testing with Selenium
-python ./my_selenium/selenium_script.py
+case ${option} in
+  nodejs)
+    # testing for Node.js App
+    node_modules/mocha/bin/mocha test.js
+    ;;
+  selenium_with_report)
+    # testing with Selenium
+    python ./my_selenium/selenium_script.py
+    ;;
+  *)
+    echo "`basename ${0}`: usage: [ nodejs ] | [ selenium_with_report ]"
+    ;;
+esac
