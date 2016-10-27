357198de5c9b2f66f0bc804eb3d9774425fcfdbe
Alan Tai <gogistics@gogistics-tw.com>
2016-10-27 11:10:42 -0700

    refactor selenium test script and test reporter

--- my_selenium/selenium_script.py
+++ my_selenium/selenium_script.py
@@ -1,30 +1,31 @@
 # -*- coding: utf-8 -*-
-import time, datetime, pytz
+import time, datetime, pytz, sys
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 from multiprocessing.dummy import Pool as ThreadsPool
+from test_reporter import TestReport
 
 
-def init_driver(tag = None):
+def init_driver_info(tag = None):
   driver_info = {}
   if tag == 'chrome':
     driver_info.update({'browser': 'Chrome'})
-    driver_info.update({'query': 'steelfusion'})
     driver = webdriver.Chrome('/Users/alantai/Desktop/my_selenium_fabric/chromedriver')
     driver.wait = WebDriverWait(driver, 5)
     driver_info.update({'webdriver': driver})
 
   elif tag == 'firefox':
     driver_info.update({'browser': 'Firefox'})
-    driver_info.update({'query': 'steelhead'})
     driver = webdriver.Firefox('/Users/alantai/Desktop/my_selenium_fabric/')
     driver.wait = WebDriverWait(driver, 5)
     driver_info.update({'webdriver': driver})
 
+  # add shared info
   driver_info.update({'url': 'http://www.riverbed.com/products/steelhead/index.html'})
+  driver_info.update({'query': 'steelfusion'})
 
   return driver_info
 
@@ -33,52 +34,31 @@ def test(info_set = None):
   query = info_set['query']
   browser = info_set['browser']
   url = info_set['url']
-
-  driver.get(url)
   timestamp = str(time.time()).split('.')[0]
-  test_file = open('./my_selenium/reports/{browser}-test-report-{timestamp}.txt'.format( browser = browser, timestamp = timestamp), 'a')
 
+  # init TestReport and start to generate report
+  test_reporter = TestReport(browser, timestamp)
+
+  # load web page
+  driver.get(url)
   print '\n\n\n'
   print '<=== Start Test Automation for {browser} ===>'.format(browser = browser)
   print '\n'
 
-  # write to report
+  # write_to_file to report
   fmt = '%Y-%m-%d %H:%M:%S %Z%z'
   # current_datetime = datetime.datetime.now(tz=pytz.utc).strftime(fmt) # for UTC
-  pdt = pytz.timezone('US/Pacific-New')
-  current_datetime = datetime.datetime.now().replace(tzinfo=pdt).strftime(fmt) # for PDT
-  test_file.write('Test Report for {browser}'.format(browser = browser))
-  test_file.write('\n')
-  test_file.write('===========')
-  test_file.write('\n')
-  test_file.write('Datetime ({pdt}): {current_datetime}'.format(pdt=pdt, current_datetime = current_datetime))
-  test_file.write('\n')
-  test_file.write('QA: {test_engineer}'.format(test_engineer='Alan Tai'))
-  test_file.write('\n\n\n')
+  pdt = pytz.timezone('US/Pacific-New') # for PDT
+  current_datetime = datetime.datetime.now().replace(tzinfo = pdt).strftime(fmt) # for PDT
+  test_engineer = 'Alan Tai'
+
+  test_reporter.write_to_file_meta(pdt, current_datetime, test_engineer)
+  test_reporter.write_to_file_title()
 
   try:
-    test_file.write('Test Cases:')
-    test_file.write('\n')
-    test_file.write('===========')
-    test_file.write('\n\n')
-        
-    # check title
-    print '<--- Check Title --->'
-    print driver.title
-    print '\n'
-    test_file.write('<--- Check Title --->')
-    test_file.write('\n')
-    test_file.write(u''.join(driver.title).encode('utf-8'))
-    test_file.write('\n\n')
+    test_reporter.write_to_file('Check Title', u''.join(driver.title).encode('utf-8'))
 
-    # check cookies
-    print '<--- Check Cookies --->'
-    print driver.get_cookies()
-    print '\n'
-    test_file.write('<--- Check Cookies --->')
-    test_file.write('\n')
-    test_file.write(driver.get_cookies().__str__())
-    test_file.write('\n\n')
+    test_reporter.write_to_file('Check Cookies', str(driver.get_cookies()))
 
     # click event
     search_btn = driver.find_element_by_xpath('//*[@id="tools"]/ul/li[3]/a')
@@ -101,60 +81,18 @@ def test(info_set = None):
     time.sleep(3)
 
     search_result = driver.find_element_by_xpath('//*[@id="totalresults"]')
-    # search_result.value_of_css_property()
-    print '<--- Check Style --->'
-    print search_result.get_attribute('style')
-    print '\n'
-    test_file.write('<--- Check Style --->')
-    test_file.write('\n')
-    test_file.write(search_result.get_attribute('style').__str__())
-    test_file.write('\n\n')
+    # search_result.value_of_css_property() # get css property
 
-    # check detail
-    print '<--- Check innerHTML --->'
-    print search_result.get_attribute('innerHTML')
-    print '\n'
-    test_file.write('<--- Check innerHTML --->')
-    test_file.write('\n')
-    test_file.write(search_result.get_attribute('innerHTML'))
-    test_file.write('\n\n')
-
-    print '<--- Check Text of Element --->'
-    print search_result.text
-    print '\n'
-    test_file.write('<--- Check Text of Element --->')
-    test_file.write('\n')
-    test_file.write(search_result.text)
-    test_file.write('\n\n')
+    test_reporter.write_to_file('Check Style', str(search_result.get_attribute('style')))
+    test_reporter.write_to_file('Check innerHTML', search_result.get_attribute('innerHTML'))
+    test_reporter.write_to_file('Check Text of Element', search_result.text)
 
     # scroll page to bottom
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
-    print '<--- Check Tag Name of Element --->'
-    print search_result.tag_name
-    print '\n'
-    test_file.write('<--- Check Tag Name of Element --->')
-    test_file.write('\n')
-    test_file.write(search_result.tag_name)
-    test_file.write('\n\n')
-
-    print '<--- Check Location of Element --->'
-    print search_result.location
-    print '\n'
-    test_file.write('<--- Check Parent of Element --->')
-    test_file.write('\n')
-    test_file.write(str(search_result.location))
-    test_file.write('\n\n')
-
-    print '<--- Check Size of Element --->'
-    print search_result.size
-    print '\n'
-    test_file.write('<--- Check Size of Element --->')
-    test_file.write('\n')
-    test_file.write(str(search_result.size))
-    test_file.write('\n\n')
-
-    time.sleep(3)
+    test_reporter.write_to_file('Check Tag Name of Element', search_result.tag_name)
+    test_reporter.write_to_file('Check Parent of Element', str(search_result.location))
+    test_reporter.write_to_file('Check Size of Element', str(search_result.size))
 
     search_btn.click()
     time.sleep(3)
@@ -162,18 +100,27 @@ def test(info_set = None):
     driver.set_window_size(768, 1024)
     time.sleep(3)
 
+    # test window switching
+    for handle in driver.window_handles:
+      driver.switch_to_window(handle)
+      time.sleep(3)
+
+
   except TimeoutException:
-    print("Errors")
+    print("Timeout!")
+  except:
+    print("Unexpected error:", sys.exc_info()[0])
 
   finally:
     time.sleep(5)
     driver.quit()
-    test_file.close()
+    test_reporter.close_file()
 
 def test_tasks():
   driver_tags = ['chrome', 'firefox']
-  drivers = [ init_driver(tag) for tag in driver_tags ]
+  drivers = [ init_driver_info(tag) for tag in driver_tags ]
 
+  # parallel testing on both Chrome and Firefox (you can add more browsers like Opera, IE, etc. and change the poll size)
   pool = ThreadsPool(2)
   results = pool.map(test, drivers)
 
---
+++ my_selenium/test_reporter.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+# incomplete
+
+class TestReport(object):
+  ''' Test Report for generating test reports '''
+  def __init__(self, browser, timestamp):
+    self.__browser = browser
+    self.__timestamp = timestamp
+    self.__test_report = open('./my_selenium/reports/{browser}-test-report-{timestamp}.txt'.format( browser = browser, timestamp = timestamp), 'a')
+
+  def write_to_file_meta(self, timezone, current_datetime, test_engineer):
+    self.__test_report.write('Test Report for {browser}'.format(browser = self.__browser))
+    self.__test_report.write('\n')
+    self.__test_report.write('==========')
+    self.__test_report.write('\n')
+    self.__test_report.write('Datetime (Time Zone: {timezone}): {current_datetime}'.format(timezone = timezone, current_datetime = current_datetime))
+    self.__test_report.write('\n')
+    self.__test_report.write('QA: {test_engineer}'.format(test_engineer = test_engineer))
+    self.__test_report.write('\n\n\n')
+
+  def write_to_file_title(self):
+    self.__test_report.write('Test Cases:')
+    self.__test_report.write('\n')
+    self.__test_report.write('==========')
+    self.__test_report.write('\n\n')
+
+  def write_to_file(self, test_case, test_info):
+    print('<--- {test_case}/{browser} --->'.format(test_case = test_case, browser = self.__browser))
+    print('\n')
+    print(test_info)
+    print('\n')
+
+    self.__test_report.write('<--- {test_case} --->'.format(test_case = test_case))
+    self.__test_report.write('\n')
+    self.__test_report.write(test_info)
+    self.__test_report.write('\n\n')
+
+  def close_file(self):
+    self.__test_report.close()
